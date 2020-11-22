874137571951d1303a5cf794f1799d2069f5b606
Nitinjmv <nitinjmv@gmail.com>
2020-11-22 21:15:01 +0530

    added crud operations

--- src/main/java/com/jmv/controller/UserController.java
+++ src/main/java/com/jmv/controller/UserController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,29 +19,55 @@ import com.jmv.repository.UserMongoRepository;
 @RestController
 @RequestMapping("users")
 public class UserController {
-	
+
 	@Autowired
 	private UserMongoRepository userMongoRepository;
 
+	// Create
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		return new ResponseEntity<>(userMongoRepository.save(user), HttpStatus.CREATED);
+	}
+
+	// Retrieve
 	@GetMapping("{id}")
-	public ResponseEntity<User> user(@PathVariable Integer id){
-		
-		User user = userMongoRepository.findById(id)
-				.orElse(null);
-		
-		return user != null 
-				? new ResponseEntity<User>(user, HttpStatus.OK) 
+	public ResponseEntity<User> retrieveUserById(@PathVariable Integer id) {
+
+		User user = userMongoRepository.findById(id).orElse(null);
+
+		return user != null ? new ResponseEntity<User>(user, HttpStatus.OK)
 				: new ResponseEntity<User>(HttpStatus.NOT_FOUND);
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<User>> users() {
+	public ResponseEntity<List<User>> retrieveAllUsers() {
 		return new ResponseEntity<List<User>>(userMongoRepository.findAll(), HttpStatus.OK);
 	}
-	
-	@PostMapping
-	public User user(@RequestBody User user){
-		return userMongoRepository.save(user);
+
+	// Delete
+	@DeleteMapping
+	public ResponseEntity<String> deleteUser(@RequestBody User user) {
+		userMongoRepository.delete(user);
+		return new ResponseEntity<String>("User " + user + " deleted", HttpStatus.OK);
 	}
-	
+
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteUserById(@PathVariable Integer id) {
+		userMongoRepository.deleteById(id);
+		return new ResponseEntity<String>("User with id: " + id + " deleted", HttpStatus.OK);
+	}
+
+	// Create
+	@PutMapping
+	public ResponseEntity<User> updateUser(@RequestBody User user) {
+		
+		User existingUser = userMongoRepository.findById(user.getId())
+		.orElse(null);
+		
+		existingUser.setEmail(user.getEmail()); 
+		existingUser.setName(user.getName()); 
+		
+		return new ResponseEntity<>(userMongoRepository.save(existingUser), HttpStatus.OK);
+	}
+
 }
